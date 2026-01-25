2cd51f027439ebe6a65a0a1c4c47dfc8002d6e0c
Run-time-Terrorist <billionsntokozomkhwanazi@gmail.com>
2026-01-25 11:31:00 +0200

    Delete Rendering directory

--- Rendering/AnimationConfig.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-constexpr int MAX_BONES = 100;
-constexpr int MAX_BONE_INFLUENCE = 4;
-
--- Rendering/AssimpAnimationLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <assimp/scene.h>
-#include "Animation.h"
-
-class AssimpAnimationLoader {
-public:
-    static Animation LoadAnimation(const aiScene* scene, const aiAnimation* aiAnim);
-};
-

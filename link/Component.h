213c59a9d4af2d26925d44d8c547e6653634e76f
RTT-Dev <billionsntokozomkhwanazi@gmail.com>
2026-02-13 15:58:53 +0200

    Delete link directory
    Signed-off-by: RTT-Dev <billionsntokozomkhwanazi@gmail.com>

--- link/Component.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <memory>
-
-// Base class for components
-struct Component {
-    virtual ~Component() = default;
-};
-
-using ComponentPtr = std::unique_ptr<Component>;
--- link/DebugDraw.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glm/glm.hpp>
-
-struct DebugLine {
-    glm::vec3 a;
-    glm::vec3 b;
-};
-
